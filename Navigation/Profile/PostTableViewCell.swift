8577ddafee0fba4f8c42a0df4169ef139e1a8d75
eva <vacumase@gmail.com>
2024-04-22 17:25:24 +0500

    add TableView

---
+++ Navigation/Profile/PostTableViewCell.swift
@@ -0,0 +1,159 @@
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    // MARK: - Subviews
+    
+    private lazy var authorLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .black
+        label.numberOfLines = 2
+
+        return label
+    }()
+    
+    private lazy var postImage: UIImageView = {
+        let image = UIImageView(image: nil)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        image.contentMode = .scaleAspectFit
+        image.backgroundColor = .black
+        return image
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .systemGray
+        label.numberOfLines = 0
+
+        return label
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .black
+
+        return label
+    }()
+    
+    private lazy var viewsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .black
+
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(
+        style: UITableViewCell.CellStyle,
+        reuseIdentifier: String?
+    ) {
+        super.init(
+            style: .subtitle,
+            reuseIdentifier: reuseIdentifier
+        )
+        
+        addSubviews()
+        
+        setupConstraints()
+
+        tuneView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        isHidden = false
+        isSelected = false
+        isHighlighted = false
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        guard let view = selectedBackgroundView else {
+            return
+        }
+        
+        contentView.insertSubview(view, at: 0)
+        selectedBackgroundView?.isHidden = !selected
+    }
+    
+    // MARK: - Private
+    
+    private func addSubviews() {
+        contentView.addSubview(authorLabel)
+        contentView.addSubview(postImage)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(likesLabel)
+        contentView.addSubview(viewsLabel)
+    }
+    
+    private func tuneView() {
+        backgroundColor = .tertiarySystemBackground
+        contentView.backgroundColor = .tertiarySystemBackground
+        textLabel?.backgroundColor = .clear
+        detailTextLabel?.backgroundColor = .clear
+        imageView?.backgroundColor = .clear
+
+        accessoryView = nil
+        accessoryType = .none
+        
+        selectionStyle = .gray
+        let selectedView = UIView()
+        selectedView.backgroundColor = .systemYellow
+        selectedBackgroundView = selectedView
+    }
+    
+    // MARK: - Public
+    
+    func update(_ model: Post) {
+        authorLabel.text = model.author
+        postImage.image = UIImage(named: model.image)
+        descriptionLabel.text = model.description
+        likesLabel.text = "Likes: \(model.likes)"
+        viewsLabel.text = "Views: \(model.views)"
+    }
+    
+    private func setupConstraints() {
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        NSLayoutConstraint.activate([
+            authorLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16.0),
+            authorLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16.0),
+//            authorLabel.trailingAnchor.constraint(equalTo:contentView.trailingAnchor),
+//            authorLabel.heightAnchor.constraint(equalToConstant: 30.0),
+//            authorLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            postImage.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 12.0),
+            postImage.heightAnchor.constraint(equalToConstant: screenSize.width),
+            postImage.widthAnchor.constraint(equalToConstant: screenSize.width),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: postImage.bottomAnchor, constant: 16.0),
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16.0),
+            
+            likesLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16.0),
+            likesLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16.0),
+            
+            viewsLabel.topAnchor.constraint(equalTo: likesLabel.topAnchor),
+            viewsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16.0)
+        ])
+    }
+}
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,49 +9,154 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    var headerView: ProfileHeaderView = {
-        var view = ProfileHeaderView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    // MARK: - Data
     
+    fileprivate let data = Post.make()
     
-    private lazy var newButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("New button", for: .normal)
-        return button
+    // MARK: - Subviews
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView.init(
+            frame: .zero,
+            style: .plain
+        )
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
     }()
 
+    private enum CellReuseID: String {
+        case base = "BaseTableViewCell_ReuseID"
+        case custom = "CustomTableViewCell_ReuseID"
+    }
+    
+    private enum HeaderFooterReuseID: String {
+        case base = "TableSectionFooterHeaderView_ReuseID"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.indexPathsForSelectedRows?.forEach{ indexPath in
+            tableView.deselectRow(
+                at: indexPath,
+                animated: animated
+            )
+        }
+    }
+
+    // MARK: - Lifecycle
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the vie
         
-        view.backgroundColor = .lightGray
-        title = "Профиль"
+        setupView()
+        addSubviews()
         
-        view.addSubview(headerView)
-        view.addSubview(newButton)
-        
+        // 1. Задаем размеры и позицию tableView
         setupConstraints()
+        
+        // 2-4.
+        tuneTableView()
+    }
+
+    
+    // MARK: - Private
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Профиль"
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
     }
     
     private func setupConstraints() {
         let safeAreaGuide = view.safeAreaLayoutGuide
         
-        NSLayoutConstraint.activate( [
-            headerView.leftAnchor.constraint(equalTo: safeAreaGuide.leftAnchor),
-            headerView.rightAnchor.constraint(equalTo: safeAreaGuide.rightAnchor),
-            headerView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 220.0),
-            
-            newButton.leftAnchor.constraint(equalTo: safeAreaGuide.leftAnchor),
-            newButton.rightAnchor.constraint(equalTo: safeAreaGuide.rightAnchor),
-            newButton.bottomAnchor.constraint(equalTo: safeAreaGuide.bottomAnchor)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaGuide.bottomAnchor),
         ])
     }
 
+    private func tuneTableView() {
+        // 2. Настраиваем отображение таблицы
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 220.0
+        if #available(iOS 15.0, *) {
+            tableView.sectionHeaderTopPadding = 0.0
+        }
+         
+        let headerView = ProfileHeaderView()
+        tableView.setAndLayout(headerView: headerView)
+        tableView.tableFooterView = UIView()
+        
+        tableView.register(
+            PostTableViewCell.self,
+            forCellReuseIdentifier: CellReuseID.base.rawValue
+        )
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
 
+extension UITableView {
+    
+    func setAndLayout(headerView: UIView) {
+        tableHeaderView = headerView
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.widthAnchor.constraint(equalTo: widthAnchor)
+        ])
+        
+        headerView.setNeedsLayout()
+        headerView.layoutIfNeeded()
+        headerView.frame.size = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+    }
+}
+
+extension ProfileViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: CellReuseID.base.rawValue,
+            for: indexPath
+        ) as? PostTableViewCell else {
+            fatalError("could not dequeueReusableCell")
+        }
+        
+        cell.update(data[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+        
+}
+
+extension ProfileViewController: UITableViewDelegate {
+    
+    func tableView(
+        _ tableView: UITableView,
+        heightForHeaderInSection section: Int
+    ) -> CGFloat {
+        UITableView.automaticDimension
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        heightForFooterInSection section: Int
+    ) -> CGFloat {
+        UITableView.automaticDimension
+    }
 }
