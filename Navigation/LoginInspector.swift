6a0cd2df967c4b08999c8ffd8ff638ed4de2321d
eva <vacumase@gmail.com>
2024-08-18 17:16:19 +0500

    completed hw

--- Navigation/LoginInspector.swift
+++ Navigation/LoginInspector.swift
@@ -1,9 +1,54 @@
+import FirebaseAuth
+
 struct LoginInspector : LoginViewControllerDelegate {
     
-    func check(with login: String, and password: String) -> Bool {
-        Checker.shared.check(with: login, and: password)
+    func check(with login: String, and password: String, completion: @escaping((Result<User, LoginError>) -> Void)) {
+        let checkerService = CheckerService()
+        checkerService.checkCredentials(login: login, pass: password) { result in
+            switch result {
+            case .success(true):
+                
+                let user = User(
+                    login: (FirebaseAuth.Auth.auth().currentUser?.email)!,
+                    fullName: (FirebaseAuth.Auth.auth().currentUser?.email)!,
+                    avatar: UIImage(resource: .avatar),
+                    status: "Я пришел с Firebase!"
+                )
+                
+                completion(.success(user))
+                
+            case .failure(.invalidEmail):
+                completion(.failure(.invalidEmail))
+            case .failure(.invalidCreds):
+                completion(.failure(.invalidCreds))
+            default:
+                completion(.failure(.invalidCreds))
+            }
+        }
     }
     
+    func singUp(with login: String, and password: String, completion: @escaping((Result<Bool, SingupError>) -> Void)) {
+        let checkerService = CheckerService()
+        checkerService.signUp(login: login, pass: password) { result in
+            switch result {
+                
+            case .success(true):
+                completion(.success(true))
+                
+            case .failure(.emailAlreadyInUse):
+                completion(.failure(.emailAlreadyInUse))
+                
+            case .failure(.invalidEmail):
+                completion(.failure(.invalidEmail))
+                
+            case .failure(.wrongPassword):
+                completion(.failure(.wrongPassword))
+                
+            default:
+                completion(.failure(.smthWentWrong))
+            }
+        }
+    }
 }
 
 protocol LoginFactory {
